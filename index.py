308a3623a6c4e7a4a4da594598b14b9f042aa6a6
menghuiguli <fujia0@qq.com>
2023-06-29 16:07:32 +0800

    get max page

--- index.py
+++ index.py
@@ -65,10 +65,26 @@ def search_items(category, page):
         item_links.append("https://www.amazon.com" + a["href"])
     return item_links
 
+#TODO get max page
+def get_total_page(content):
+    # url = f"https://www.amazon.com/s?k={category}"
+    # response = requests.get(url, headers=headers, proxies=proxies)
+    
+    # if response.status_code != 200:
+    #     print("request failed")
+    #     sys.exit()
+    
+    # soup = BeautifulSoup(response.text, "html.parser")
+    
+    # page = soup.find_all("div", {"data-component-type": "s-search-result"})
+    
+    return 6
+    
 
-def scrapy_items(category, page):
+def scrapy_items(category):
     total_links = []
 
+    page = get_total_page()
     # 循环页面抓取商品
     for i in range(1, page):
         item_links = search_items(category, i)
@@ -94,33 +110,9 @@ def scrapy_items(category, page):
     with open("result.txt", "w") as outfile:
         outfile.write("\n".join(result_links))
 
+
 if __name__ == "__main__":
     # 测试
     category = "chairs"  # 请将此处替换为您感兴趣的商品类别
 
-    total_links = []
-
-    # 循环页面抓取商品
-    for i in range(1, 6):
-        item_links = search_items(category, i)
-        total_links.extend(item_links)
-        random_sleep()
-
-    result_links = []
-
-    if not total_links:
-        print("failed to scrapy")
-
-    rs = (grequests.get(u, headers=headers, proxies=proxies) for u in total_links)
-
-    responses = grequests.map(rs, size=10)
-
-    # Iterate over the responses and print the results.
-    for response in responses:
-        if response is not None:
-            if check_availability(response.text):
-                result_links.append(response.url)
-
-    print(result_links)
-    with open("result.txt", "w") as outfile:
-        outfile.write("\n".join(result_links))
+    scrapy_items(category)
